4699ba3dc74956c248dd26906c4d01783165576b
toth8j <toth.jozsef8@mav-szk.hu>
2017-05-31 16:20:36 +0200

    HTML5_4 feltöltése 5. nap

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Canvas kitöltése
+function fillCanvas() {
+    var canvas = document.querySelector("#myCanvas");
+    var context = canvas.getContext("2d"); //kétdimenzióban szeretnénk rajzolni
+    context.fillStyle = "#ff0000";
+    context.fillRect(10, 10, 180, 80);
+}
+
+//fillCanvas();
+
+// Kép betöltése.
+function fillImg() {
+    var canvas = document.querySelector("#myCanvas");
+    var context = canvas.getContext("2d"); //kétdimenzióban szeretnénk rajzolni
+    
+    //Vonal rajzolása
+    context.moveTo(100, 100);
+    context.lineTo(300, 100);
+    context.stroke();
+
+    //Kör rajzolása
+    context.beginPath();
+    context.arc(200, 50, 40, 0, 2*Math.PI);
+    context.stroke();
+    
+    //Kép kiválasztása
+    var img = document.querySelector("#myImg");
+    context.drawImage(img, 0, 0);
+    
+}
+
+fillImg();
